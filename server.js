55bc4ccd70b6fb3ab542da6b8ebaa3044bedc6fd
Koby Whitaker <kpwhit714@gmail.com>
2023-04-28 13:33:22 -0400

    Add parameter endpoint support

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const port = args.port ? args.port : 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// GET routes
 app.get('/app/', (req, res) => {
     res.status(200);
     res.send('200 OK');
@@ -32,7 +33,15 @@ app.get('/app/rpsls/play/', (req, res) => {
     res.send(rpsls.rpsls(req.query.shot))
 });
 
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.send(rpsls.rps(req.params.shot));
+});
 
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.send(rpsls.rpsls(req.params.shot));
+});
+
+// POST routes
 app.post('/app/rps/play/', (req, res) => {
     res.send(rpsls.rps(req.body.shot));
 });
@@ -40,7 +49,6 @@ app.post('/app/rps/play/', (req, res) => {
 app.post('/app/rpsls/play/', (req, res) => {
     res.send(rpsls.rpsls(req.body.shot));
 })
-
 
 // All other (invalid) routes here
 app.get('*', (req, res) => {
