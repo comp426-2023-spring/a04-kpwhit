c47cddcc2e70b7176f93ca6f797875d5a4724592
Koby Whitaker <kpwhit714@gmail.com>
2023-04-28 10:04:35 -0400

    Add rps and rpsls GET requests

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const minimist = require('minimist');
 const args = minimist(process.argv.slice(2));
+const rpsls = require('./lib/lib/rpsls')
 
 const app = express();
 const port = args.port ? args.port : 5000;
@@ -10,9 +11,15 @@ const port = args.port ? args.port : 5000;
 app.get('/app/', (req, res) => {
     res.status(200);
     res.send('200 OK');
-})
+});
 
+app.get('/app/rps/', (req, res) => {
+    res.send(rpsls.rps());
+});
 
+app.get('/app/rpsls/', (req, res) => {
+    res.send(rpsls.rpsls());
+});
 
 
 
@@ -20,6 +27,6 @@ app.get('/app/', (req, res) => {
 app.get('*', (req, res) => {
     res.status(404);
     res.send('404 NOT FOUND');
-})
+});
 
 app.listen(port);
